623ed4fdf5cc715241033646739638e76702c489
Mukul Dave <mhdave@lbl.gov>
2024-08-07 19:29:10 -0500

    complete porting jacobi to rccl

--- apps/rccl/jacobi_rccl.cpp
+++ apps/rccl/jacobi_rccl.cpp
@@ -3,6 +3,12 @@
 #include "commons/hip.hpp"
 #include "commons/rccl.hpp"
 
+// #define HAVE_ROCPRIM
+
+#ifdef HAVE_ROCPRIM
+#include <rocprim/block/block_reduce.hpp>
+#endif
+
 #ifdef USE_DOUBLE
 typedef double real;
 #define MPI_REAL_TYPE MPI_DOUBLE
@@ -15,6 +21,73 @@ typedef float real;
 
 constexpr real tol = 1.0e-8;
 const real PI = 2.0 * std::asin(1.0);
+
+__global__ void initialize_boundaries(real* __restrict__ const a_new, real* __restrict__ const a,
+                                      const real pi, const int offset, const int nx,
+                                      const int my_ny, const int ny) {
+    for (int iy = blockIdx.x * blockDim.x + threadIdx.x; iy < my_ny; iy += blockDim.x * gridDim.x) {
+        const real y0 = sin(2.0 * pi * (offset + iy) / (ny - 1));
+        a[iy * nx + 0] = y0;
+        a[iy * nx + (nx - 1)] = y0;
+        a_new[iy * nx + 0] = y0;
+        a_new[iy * nx + (nx - 1)] = y0;
+    }
+}
+
+void launch_initialize_boundaries(real* __restrict__ const a_new, real* __restrict__ const a,
+                                  const real pi, const int offset, const int nx, const int my_ny,
+                                  const int ny) {
+    initialize_boundaries<<<my_ny / 128 + 1, 128>>>(a_new, a, pi, offset, nx, my_ny, ny);
+    CHECK_HIP(hipGetLastError());
+}
+
+template <int BLOCK_DIM_X, int BLOCK_DIM_Y>
+__global__ void jacobi_kernel(real* __restrict__ const a_new, const real* __restrict__ const a,
+                              real* __restrict__ const l2_norm, const int iy_start,
+                              const int iy_end, const int nx, const bool calculate_norm) {
+#ifdef HAVE_ROCPRIM
+    using BlockReduce = rocprim::block_reduce<real, BLOCK_DIM_X,
+            rocprim::block_reduce_algorithm::using_warp_reduce, BLOCK_DIM_Y>;
+    __shared__ typename BlockReduce::storage_type temp_storage;
+#endif  // HAVE_ROCPRIM
+
+    int iy = blockIdx.y * blockDim.y + threadIdx.y + iy_start;
+    int ix = blockIdx.x * blockDim.x + threadIdx.x + 1;
+    real local_l2_norm = 0.0;
+
+    if (iy < iy_end && ix < (nx - 1)) {
+        const real new_val = 0.25 * (a[iy * nx + ix + 1] + a[iy * nx + ix - 1] +
+                                     a[(iy + 1) * nx + ix] + a[(iy - 1) * nx + ix]);
+        a_new[iy * nx + ix] = new_val;
+        if (calculate_norm) {
+            real residue = new_val - a[iy * nx + ix];
+            local_l2_norm += residue * residue;
+        }
+    }
+    if (calculate_norm) {
+#ifdef HAVE_ROCPRIM
+    real block_l2_norm;
+    BlockReduce().reduce(local_l2_norm, block_l2_norm, temp_storage);
+    if (0 == threadIdx.y && 0 == threadIdx.x) atomicAdd(l2_norm, block_l2_norm);
+#else
+    atomicAdd(l2_norm, local_l2_norm);
+#endif  // HAVE_ROCPRIM
+    }
+}
+
+void launch_jacobi_kernel(real* __restrict__ const a_new, const real* __restrict__ const a,
+                          real* __restrict__ const l2_norm, const int iy_start, const int iy_end,
+                          const int nx, const bool calculate_norm, hipStream_t stream) {
+    constexpr int dim_block_x = 32;
+    constexpr int dim_block_y = 32;
+    dim3 dim_grid((nx + dim_block_x - 1) / dim_block_x,
+                  ((iy_end - iy_start) + dim_block_y - 1) / dim_block_y, 1);
+    dim3 dim_block = {dim_block_x, dim_block_y, 1};
+    jacobi_kernel<dim_block_x, dim_block_y><<<dim_grid, dim_block, 0, stream>>>(
+        a_new, a, l2_norm, iy_start, iy_end, nx, calculate_norm);
+    CHECK_HIP(hipGetLastError());
+}
+
 
 double single_gpu(const int nx, const int ny, const int iter_max, real* const a_ref_h,
                   const int nccheck, const bool print);
@@ -138,7 +211,156 @@ int main(int argc, char* argv[]) {
     launch_initialize_boundaries(a, a_new, PI, iy_start_global - 1, nx, (chunk_size + 2), ny);
     CHECK_HIP(hipDeviceSynchronize());
 
+    int leastPriority = 0;
+    int greatestPriority = leastPriority;
+    CHECK_HIP(hipDeviceGetStreamPriorityRange(&leastPriority, &greatestPriority));
+    hipStream_t compute_stream;
+    CHECK_HIP(hipStreamCreateWithPriority(&compute_stream, hipStreamDefault, leastPriority));
+    hipStream_t push_stream;
+    CHECK_HIP(
+        hipStreamCreateWithPriority(&push_stream, hipStreamDefault, greatestPriority));
+
+    hipEvent_t push_prep_done;
+    CHECK_HIP(hipEventCreateWithFlags(&push_prep_done, hipEventDisableTiming));
+    hipEvent_t push_done;
+    CHECK_HIP(hipEventCreateWithFlags(&push_done, hipEventDisableTiming));
+    hipEvent_t reset_l2norm_done;
+    CHECK_HIP(hipEventCreateWithFlags(&reset_l2norm_done, hipEventDisableTiming));
+
+    real* l2_norm_d;
+    CHECK_HIP(hipMalloc(&l2_norm_d, sizeof(real)));
+    real* l2_norm_h;
+    CHECK_HIP(hipHostMalloc(&l2_norm_h, sizeof(real)));
+
+    // PUSH_RANGE("NCCL_Warmup", 5)
+    for (int i = 0; i < 10; ++i) {
+        const int top = rank > 0 ? rank - 1 : (size - 1);
+        const int bottom = (rank + 1) % size;
+        NCCL_CALL(ncclGroupStart());
+        NCCL_CALL(ncclRecv(a_new,                     nx, NCCL_REAL_TYPE, top,    nccl_comm, compute_stream));
+        NCCL_CALL(ncclSend(a_new + (iy_end - 1) * nx, nx, NCCL_REAL_TYPE, bottom, nccl_comm, compute_stream));
+        NCCL_CALL(ncclRecv(a_new + (iy_end * nx),     nx, NCCL_REAL_TYPE, bottom, nccl_comm, compute_stream));
+        NCCL_CALL(ncclSend(a_new + iy_start * nx,     nx, NCCL_REAL_TYPE, top,    nccl_comm, compute_stream));
+        NCCL_CALL(ncclGroupEnd());
+        CHECK_HIP(hipStreamSynchronize(compute_stream));
+        std::swap(a_new, a);
+    }
+    // POP_RANGE
+
+    CHECK_HIP(hipDeviceSynchronize());
+
+    if (!csv && 0 == rank) {
+        printf(
+            "Jacobi relaxation: %d iterations on %d x %d mesh with norm check "
+            "every %d iterations\n",
+            iter_max, ny, nx, nccheck);
+    }
+
+    int iter = 0;
+    bool calculate_norm = true;
+    real l2_norm = 1.0;
+
+    MPI_CHECK(MPI_Barrier(MPI_COMM_WORLD));
+    double start = MPI_Wtime();
+    // PUSH_RANGE("Jacobi solve", 0)
+    while (l2_norm > tol && iter < iter_max) {
+        CHECK_HIP(hipMemsetAsync(l2_norm_d, 0, sizeof(real), compute_stream));
+        CHECK_HIP(hipEventRecord(reset_l2norm_done, compute_stream));
+
+        CHECK_HIP(hipStreamWaitEvent(push_stream, reset_l2norm_done, 0));
+        calculate_norm = (iter % nccheck) == 0 || (!csv && (iter % 100) == 0);
+        launch_jacobi_kernel(a_new, a, l2_norm_d, iy_start, (iy_start + 1), nx, calculate_norm,
+                             push_stream);
+
+        launch_jacobi_kernel(a_new, a, l2_norm_d, (iy_end - 1), iy_end, nx, calculate_norm,
+                             push_stream);
+        CHECK_HIP(hipEventRecord(push_prep_done, push_stream));
+
+        launch_jacobi_kernel(a_new, a, l2_norm_d, (iy_start + 1), (iy_end - 1), nx, calculate_norm,
+                             compute_stream);
+
+        if (calculate_norm) {
+            CHECK_HIP(hipStreamWaitEvent(compute_stream, push_prep_done, 0));
+            CHECK_HIP(hipMemcpyAsync(l2_norm_h, l2_norm_d, sizeof(real), hipMemcpyDeviceToHost,
+                                         compute_stream));
+        }
+
+        const int top = rank > 0 ? rank - 1 : (size - 1);
+        const int bottom = (rank + 1) % size;
+
+        // Apply periodic boundary conditions
+        // PUSH_RANGE("NCCL_LAUNCH", 5)
+        NCCL_CALL(ncclGroupStart());
+        NCCL_CALL(ncclRecv(a_new,                     nx, NCCL_REAL_TYPE, top,    nccl_comm, push_stream));
+        NCCL_CALL(ncclSend(a_new + (iy_end - 1) * nx, nx, NCCL_REAL_TYPE, bottom, nccl_comm, push_stream));
+        NCCL_CALL(ncclRecv(a_new + (iy_end * nx),     nx, NCCL_REAL_TYPE, bottom, nccl_comm, push_stream));
+        NCCL_CALL(ncclSend(a_new + iy_start * nx,     nx, NCCL_REAL_TYPE, top,    nccl_comm, push_stream));
+        NCCL_CALL(ncclGroupEnd());
+        CHECK_HIP(hipEventRecord(push_done, push_stream));
+        // POP_RANGE
+
+        if (calculate_norm) {
+            CHECK_HIP(hipStreamSynchronize(compute_stream));
+            MPI_CHECK(MPI_Allreduce(l2_norm_h, &l2_norm, 1, MPI_REAL_TYPE, MPI_SUM, MPI_COMM_WORLD));
+            l2_norm = std::sqrt(l2_norm);
+
+            if (!csv && 0 == rank && (iter % 100) == 0) {
+                printf("%5d, %0.6f\n", iter, l2_norm);
+            }
+        }
+        CHECK_HIP(hipStreamWaitEvent(compute_stream, push_done, 0));
+
+        std::swap(a_new, a);
+        iter++;
+    }
+    CHECK_HIP(hipDeviceSynchronize());
+    double stop = MPI_Wtime();
+    // POP_RANGE
+
+    CHECK_HIP(hipMemcpy(a_h + iy_start_global * nx, a + nx,
+                            std::min((ny - iy_start_global) * nx, chunk_size * nx) * sizeof(real),
+                            hipMemcpyDeviceToHost));
+
+
     int result_correct = 1;
+    for (int iy = iy_start_global; result_correct && (iy < iy_end_global); ++iy) {
+        for (int ix = 1; result_correct && (ix < (nx - 1)); ++ix) {
+            if (std::fabs(a_ref_h[iy * nx + ix] - a_h[iy * nx + ix]) > tol) {
+                fprintf(stderr,
+                        "ERROR on rank %d: a[%d * %d + %d] = %f does not match %f "
+                        "(reference)\n",
+                        rank, iy, nx, ix, a_h[iy * nx + ix], a_ref_h[iy * nx + ix]);
+                result_correct = 0;
+            }
+        }
+    }
+
+    int global_result_correct = 1;
+    MPI_CHECK(MPI_Allreduce(&result_correct, &global_result_correct, 1, MPI_INT, MPI_MIN,
+                           MPI_COMM_WORLD));
+    result_correct = global_result_correct;
+
+    if (rank == 0 && result_correct) {
+        if (csv) {
+            printf("nccl_overlap, %d, %d, %d, %d, %d, 1, %f, %f\n", nx, ny, iter_max, nccheck, size,
+                   (stop - start), runtime_serial);
+        } else {
+            printf("Num GPUs: %d.\n", size);
+            printf(
+                "%dx%d: 1 GPU: %8.4f s, %d GPUs: %8.4f s, speedup: %8.2f, "
+                "efficiency: %8.2f \n",
+                ny, nx, runtime_serial, size, (stop - start), runtime_serial / (stop - start),
+                runtime_serial / (size * (stop - start)) * 100);
+        }
+    }
+    CHECK_HIP(hipEventDestroy(reset_l2norm_done));
+    CHECK_HIP(hipEventDestroy(push_done));
+    CHECK_HIP(hipEventDestroy(push_prep_done));
+    CHECK_HIP(hipStreamDestroy(push_stream));
+    CHECK_HIP(hipStreamDestroy(compute_stream));
+
+    CHECK_HIP(hipHostFree(l2_norm_h));
+    CHECK_HIP(hipFree(l2_norm_d));
 
     CHECK_HIP(hipFree(a_new));
     CHECK_HIP(hipFree(a));
@@ -158,31 +380,39 @@ double single_gpu(const int nx, const int ny, const int iter_max, real* const a_
     real* a_new;
 
     hipStream_t compute_stream;
+    hipStream_t push_top_stream;
+    hipStream_t push_bottom_stream;
+    hipEvent_t compute_done;
+    hipEvent_t push_top_done;
+    hipEvent_t push_bottom_done;
 
     real* l2_norm_d;
     real* l2_norm_h;
 
     int iy_start = 1;
-    int iy_end = ny - 3;
+    int iy_end = (ny - 1);
 
-    auto status = hipMalloc((void**)&a, nx * ny * sizeof(real));
-    status = hipMalloc((void**)&a_new, nx * ny * sizeof(real));
+    CHECK_HIP(hipMalloc(&a, nx * ny * sizeof(real)));
+    CHECK_HIP(hipMalloc(&a_new, nx * ny * sizeof(real)));
 
-    status = hipMemset(a, 0, nx * ny * sizeof(real));
-    status = hipMemset(a_new, 0, nx * ny * sizeof(real));
+    CHECK_HIP(hipMemset(a, 0, nx * ny * sizeof(real)));
+    CHECK_HIP(hipMemset(a_new, 0, nx * ny * sizeof(real)));
 
     // Set diriclet boundary conditions on left and right boarder
-    initialize_boundaries<<<ny / 128 + 1, 128>>>(a, a_new, PI, 0, nx, ny - 2, ny - 2);
+    launch_initialize_boundaries(a, a_new, PI, 0, nx, ny, ny);
+    CHECK_HIP(hipDeviceSynchronize());
 
-    status = hipGetLastError();
-    status = hipDeviceSynchronize();
+    CHECK_HIP(hipStreamCreate(&compute_stream));
+    CHECK_HIP(hipStreamCreate(&push_top_stream));
+    CHECK_HIP(hipStreamCreate(&push_bottom_stream));
+    CHECK_HIP(hipEventCreateWithFlags(&compute_done, hipEventDisableTiming));
+    CHECK_HIP(hipEventCreateWithFlags(&push_top_done, hipEventDisableTiming));
+    CHECK_HIP(hipEventCreateWithFlags(&push_bottom_done, hipEventDisableTiming));
 
-    status = hipStreamCreate(&compute_stream);
+    CHECK_HIP(hipMalloc(&l2_norm_d, sizeof(real)));
+    CHECK_HIP(hipHostMalloc(&l2_norm_h, sizeof(real)));
 
-    status = hipMalloc(&l2_norm_d, sizeof(real));
-    status = hipHostMalloc(&l2_norm_h, sizeof(real));
-
-    status = hipDeviceSynchronize();
+    CHECK_HIP(hipDeviceSynchronize());
 
     if (print)
         printf(
@@ -191,31 +421,42 @@ double single_gpu(const int nx, const int ny, const int iter_max, real* const a_
             "check every %d iterations\n",
             iter_max, ny, nx, nccheck);
 
-    constexpr int dim_block_x = 1024;
-    constexpr int dim_block_y = 1;
-    dim3 dim_grid((nx + dim_block_x - 1) / dim_block_x, ((ny - 2) + dim_block_y - 1) / dim_block_y,
-                  1);
-
     int iter = 0;
+    bool calculate_norm = true;
     real l2_norm = 1.0;
 
-    status = hipDeviceSynchronize();
     double start = MPI_Wtime();
     // PUSH_RANGE("Jacobi solve", 0)
-
     while (l2_norm > tol && iter < iter_max) {
-        status = hipMemsetAsync(l2_norm_d, 0, sizeof(real), compute_stream);
+        CHECK_HIP(hipMemsetAsync(l2_norm_d, 0, sizeof(real), compute_stream));
 
-        dim3 dim_block = {dim_block_x, dim_block_y, 1};
-        jacobi_kernel<dim_block_x, dim_block_y>
-            <<<dim_grid, dim_block, 0, compute_stream>>>(
-                a_new, a, l2_norm_d, iy_start, iy_end, nx, mype, iy_end + 1, mype, (iy_start - 1));
-        status = hipGetLastError();
+        CHECK_HIP(hipStreamWaitEvent(compute_stream, push_top_done, 0));
+        CHECK_HIP(hipStreamWaitEvent(compute_stream, push_bottom_done, 0));
 
-        if ((iter % nccheck) == 0 || (print && ((iter % 100) == 0))) {
-            status = hipMemcpyAsync(l2_norm_h, l2_norm_d, sizeof(real), hipMemcpyDeviceToHost,
-                                         compute_stream);
-            status = hipStreamSynchronize(compute_stream);
+        calculate_norm = (iter % nccheck) == 0 || (iter % 100) == 0;
+        launch_jacobi_kernel(a_new, a, l2_norm_d, iy_start, iy_end, nx, calculate_norm,
+                             compute_stream);
+        CHECK_HIP(hipEventRecord(compute_done, compute_stream));
+
+        if (calculate_norm) {
+            CHECK_HIP(hipMemcpyAsync(l2_norm_h, l2_norm_d, sizeof(real), hipMemcpyDeviceToHost,
+                                         compute_stream));
+        }
+
+        // Apply periodic boundary conditions
+
+        CHECK_HIP(hipStreamWaitEvent(push_top_stream, compute_done, 0));
+        CHECK_HIP(hipMemcpyAsync(a_new, a_new + (iy_end - 1) * nx, nx * sizeof(real),
+                                     hipMemcpyDeviceToDevice, push_top_stream));
+        CHECK_HIP(hipEventRecord(push_top_done, push_top_stream));
+
+        CHECK_HIP(hipStreamWaitEvent(push_bottom_stream, compute_done, 0));
+        CHECK_HIP(hipMemcpyAsync(a_new + iy_end * nx, a_new + iy_start * nx, nx * sizeof(real),
+                                     hipMemcpyDeviceToDevice, compute_stream));
+        CHECK_HIP(hipEventRecord(push_bottom_done, push_bottom_stream));
+
+        if (calculate_norm) {
+            CHECK_HIP(hipStreamSynchronize(compute_stream));
             l2_norm = *l2_norm_h;
             l2_norm = std::sqrt(l2_norm);
             if (print && (iter % 100) == 0) printf("%5d, %0.6f\n", iter, l2_norm);
@@ -224,18 +465,22 @@ double single_gpu(const int nx, const int ny, const int iter_max, real* const a_
         std::swap(a_new, a);
         iter++;
     }
-    status = hipDeviceSynchronize();
     // POP_RANGE
     double stop = MPI_Wtime();
 
-    status = hipMemcpy(a_ref_h, a, nx * ny * sizeof(real), hipMemcpyDeviceToHost);
+    CHECK_HIP(hipMemcpy(a_ref_h, a, nx * ny * sizeof(real), hipMemcpyDeviceToHost));
 
-    status = hipStreamDestroy(compute_stream);
+    CHECK_HIP(hipEventDestroy(push_bottom_done));
+    CHECK_HIP(hipEventDestroy(push_top_done));
+    CHECK_HIP(hipEventDestroy(compute_done));
+    CHECK_HIP(hipStreamDestroy(push_bottom_stream));
+    CHECK_HIP(hipStreamDestroy(push_top_stream));
+    CHECK_HIP(hipStreamDestroy(compute_stream));
 
-    status = hipHostFree(l2_norm_h);
-    status = hipFree(l2_norm_d);
+    CHECK_HIP(hipHostFree(l2_norm_h));
+    CHECK_HIP(hipFree(l2_norm_d));
 
-    status = hipFree(a_new);
-    status = hipFree(a);
+    CHECK_HIP(hipFree(a_new));
+    CHECK_HIP(hipFree(a));
     return (stop - start);
 }
